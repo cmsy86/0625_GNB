9fddd50cb5841684bbceb2e6ffdfe256bf211deb
cmsy86 <cmsy86@naver.com>
2021-08-08 14:25:08 +0900

    0625_GNB

---
+++ part03_가로배경형/script/script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $("li.mainLi").mouseover(function(){
+        $("ul.subMenu").stop().slideDown(800);
+        $("div#gnbBG").stop().slideDown(800);
+    });
+
+    $("li.mainLi").mouseout(function(){
+        $("ul.subMenu").stop().slideUp(300);
+        $("div#gnbBG").stop().slideUp(300);
+    });
+
+});
---
+++ part04_세로1열형/script/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    
+    $("li.mainLi").mouseover(function(){
+        $(this).children("ul.subMenu").stop().slideDown(300);
+    });
+    
+        $("li.mainLi").mouseout(function(){
+        $(this).children("ul.subMenu").stop().slideUp(800);
+    });
+    
+});
